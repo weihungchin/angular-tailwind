f0ba142613fabe7fce78b6256fa86c76cf287ba1
WeiHung <wei_hung.chin@daimler.com>
2021-04-20 15:33:41 +0800

    Add tailwind css with optimisation

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/**/*.{html,ts}'], // add this
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
